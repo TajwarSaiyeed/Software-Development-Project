2297877f9e5fbcc2219cc894377f91feaa1d1851
tajwarsaiyeed <rjabid36@gmail.com>
2024-10-27 13:57:35 +0600

    Refactor p9.js to calculate monthly savings accurately

--- module6.5/p9.js
+++ module6.5/p9.js
@@ -6,7 +6,7 @@ const monthlySavings = (AllPayments, livingCost) => {
   let total = AllPayments.reduce((a, b) => a + b, 0);
   let tax = 0;
   AllPayments.forEach((tk) => {
-    if (tk > 3000) tax += tk * 0.2;
+    if (tk >= 3000) tax += tk * 0.2;
   });
 
   let savings = total - tax - livingCost;
@@ -15,7 +15,7 @@ const monthlySavings = (AllPayments, livingCost) => {
   else return savings;
 };
 
-const AllPayments = [1000, 2000, 3000, 4000, 5000];
-const livingCost = 5000;
+const AllPayments = [1000, 2000, 2500]
+const livingCost = 5000
 
 console.log(monthlySavings(AllPayments, livingCost));
